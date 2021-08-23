2a55c6da92b0a3f7dd0ac0c6431d1ba7817d53e4
EC2 Default User <ec2-user@ip-172-31-45-131.ap-northeast-1.compute.internal>
2021-08-23 02:25:51 +0000

    Retrieve api key from secrets manager.

--- demo-app/src/components/HelloWorld.vue
+++ demo-app/src/components/HelloWorld.vue
@@ -12,29 +12,64 @@
 </template>
 
 <script>
+import axios from 'axios'
+import AWS from 'aws-sdk'
+var region = 'us-east-1'
+
+AWS.config.region = region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'us-east-1:0a2f0884-bdf3-49e7-93ee-05754346c345'
+})
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Secrets Manager Demo',
       pets: [],
-      api_endpoint: 'https://ltxtx28wic.execute-api.ap-northeast-1.amazonaws.com/prod/pets/',
-      api_key: '4goMJWKP3rK75P6qb8sn9ARsCec2mkT96ig6iS50'
+      api_endpoint: 'https://ltxtx28wic.execute-api.ap-northeast-1.amazonaws.com/prod/pets/'
     }
   },
   mounted () {
     var $this = this
-    this.$axios
-      .get(this.api_endpoint, {
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': this.api_key
-        },
-        data: {}
-      })
-      .then(response => {
-        $this.pets = response.data
-      })
+    AWS.config.credentials.get(function (err) {
+      if (err) {
+        console.log(err)
+      } else {
+        var secretName = 'demo/api_key'
+        var client = new AWS.SecretsManager({
+          region: region
+        })
+
+        client.getSecretValue({SecretId: secretName}, function (err, data) {
+          if (err) {
+            console.log(err)
+          } else {
+            var secret
+            if ('SecretString' in data) {
+              secret = data.SecretString
+            } else {
+              let buff = Buffer.from(data.SecretBinary, 'base64')
+              secret = buff.toString('ascii')
+            }
+            secret = JSON.parse(secret)
+            console.log(secret)
+            console.log(secret.DEMO_API_KEY)
+            axios
+              .get($this.api_endpoint, {
+                headers: {
+                  'Content-Type': 'application/json',
+                  'x-api-key': secret.DEMO_API_KEY
+                },
+                data: {}
+              })
+              .then(response => {
+                $this.pets = response.data
+              })
+          }
+        })
+      }
+    })
   }
 }
 </script>
--- demo-app/src/main.js
+++ demo-app/src/main.js
@@ -2,10 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import axios from 'axios'
 
 Vue.config.productionTip = false
-Vue.prototype.$axios = axios
 
 /* eslint-disable no-new */
 new Vue({
